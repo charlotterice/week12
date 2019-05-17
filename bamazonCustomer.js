94001e7bb5de4c10cf49ec28e432b01423e8caaa
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-05-16 22:19:14 -0400

    updates

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,15 +4,21 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "Westpoint19!",
   database: "bamazon"
 });
 
-connection.connect();
- 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  beginSale();
 });
+
+function beginSale(){
+connection.query("SELECT * FROM products", function (error, results, fields) {
+  if (error) throw error;
+  console.log("Welcome to my Bamazon Store");
+  connection.end();
+});
+}
  
-connection.end();
